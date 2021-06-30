b30a3e3831bbfeb42b2998ef3db204fd5b5c65f7
Caio Augusto <65203015+llBlueLighting@users.noreply.github.com>
2021-06-30 15:47:21 -0300

    Add files via upload

---
+++ GiraSolar_Tracker[1].ino
@@ -0,0 +1,94 @@
+/* 
+Programa: GiraSolar_Tracker
+*/
+
+#include <Servo.h>
+//Declarando Servos Motores
+Servo servohori;
+int servoh = 0;
+int servohLimitHigh = 160;
+int servohLimitLow = 20;
+
+Servo servoverti; 
+int servov = 0; 
+int servovLimitHigh = 160;
+int servovLimitLow = 20;
+//Declarando LDR's 
+int ldrtopl = 2; //Superior Esquerdo LDR
+int ldrtopr = 1; //Superior Direito LDR
+int ldrbotl = 3; //Inferior Direito LDR
+int ldrbotr = 0; //Inferior Esquerdo LDR
+
+ void setup () 
+ {
+  servohori.attach(10);
+  servohori.write(0);
+  servoverti.attach(9);
+  servoverti.write(0);
+  delay(500);
+ }
+
+void loop()
+{
+  servoh = servohori.read();
+  servov = servoverti.read();
+  //Ler valores de cada LDR
+  int topl = analogRead(ldrtopl);
+  int topr = analogRead(ldrtopr);
+  int botl = analogRead(ldrbotl);
+  int botr = analogRead(ldrbotr);
+  //Media dos valores dos LDR's
+  int avgtop = (topl + topr) / 2; 
+  int avgbot = (botl + botr) / 2; 
+  int avgleft = (topl + botl) / 2;
+  int avgright = (topr + botr) / 2;
+
+   //Orientação dos servos com base no       resultado das médias
+ 
+     if (avgtop < avgbot)
+  {
+    servoverti.write(servov +1);
+    if (servov > servovLimitHigh) 
+     { 
+      servov = servovLimitHigh;
+     }
+    delay(10);
+  }
+  else if (avgbot < avgtop)
+  {
+    servoverti.write(servov -1);
+    if (servov < servovLimitLow)
+  {
+    servov = servovLimitLow;
+  }
+    delay(10);
+  }
+  else 
+  {
+    servoverti.write(servov);
+  }
+  
+  if (avgleft > avgright)
+  {
+    servohori.write(servoh +1);
+    if (servoh > servohLimitHigh)
+    {
+    servoh = servohLimitHigh;
+    }
+    delay(10);
+  }
+  else if (avgright > avgleft)
+  {
+    servohori.write(servoh -1);
+    if (servoh < servohLimitLow)
+     {
+     servoh = servohLimitLow;
+     }
+    delay(10);
+  }
+  else 
+  {
+    servohori.write(servoh);
+  }
+  delay(50);
+}
